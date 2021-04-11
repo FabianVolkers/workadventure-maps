f127199c57bad0bae012e2d6fdcfdc0226ee892d
Fabian Volkers <fabian.volkers@gmail.com>
2021-04-11 22:48:37 +0200

    update map

--- Factory/Outside/metal.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="metal" tilewidth="32" tileheight="32" tilecount="6" columns="2">
- <image source="../../assets/metal.png" width="88" height="96"/>
-</tileset>
