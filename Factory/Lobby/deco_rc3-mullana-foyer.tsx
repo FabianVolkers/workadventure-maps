a330e57e1d36082df9aff712aadbeaaa29fc0a76
jdew <mail@jdew.de>
2021-04-11 23:19:55 +0200

    changed loby

---
+++ Factory/Lobby/deco_rc3-mullana-foyer.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="deco_rc3-mullana-foyer" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="../assets/deco_rc3-mullana-foyer.png" width="1024" height="1024"/>
+</tileset>
